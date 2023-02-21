605f43043cec3ac10ee588617d43bc0a77aa4749
Yash Vakil <yashvakil96@gmail.com>
2023-02-21 17:52:55 +0530

    New Data

--- Assets/Main/TestLab/TimelineScript.cs
+++ Assets/Main/TestLab/TimelineScript.cs
@@ -1,17 +1,38 @@
-using System;
+using Cinemachine;
 using UnityEngine;
 using UnityEngine.Playables;
 using UnityEngine.Rendering;
 
 public class TimelineScript : MonoBehaviour
 {
+    public GameObject freeLookCam;
     public PlayableDirector timeline;
+    public PlayableDirector antiBody;
     public double prevValue = 0;
     public FreeCamera freeCam;
+    public Camera cam;
+    public CinemachineFreeLook bacteriaCam;
+    public EnableCam enableCam;
+
+
+    public void BacteriaCamButton()
+    {
+        freeLookCam.SetActive(false);
+        enableCam.freeLook = bacteriaCam;
+        bacteriaCam.enabled = true;
+    }
 
     public void PlayTimeline()
     {
+        freeLookCam.SetActive(false);
+        cam.transform.localPosition = Vector3.zero;
+        cam.transform.localRotation = Quaternion.Euler(0,0,0);
         timeline.Play();
+    }
+
+    public void PlayAntiBody()
+    {
+        antiBody.Play();
     }
 
     private void Update()
