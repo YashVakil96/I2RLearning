489cd7a9635c80a9d11a99a413e667745ae50688
Yash Vakil <yashvakil96@gmail.com>
2023-02-22 13:28:48 +0530

    Fade Color Implimented

--- Assets/Main/TestLab/ThingsToHide.cs
+++ Assets/Main/TestLab/ThingsToHide.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,12 +7,42 @@ public class ThingsToHide : MonoBehaviour
 {
     public List<GameObject> parts;
     public List<GameObject> texts;
+    public List<Material> materialsToFade;
+    public Color fade;
     public int cycleState = 0;
 
     private void Start()
     {
         Hide(cycleState);
     }
+
+
+    IEnumerator DoAThingOverTime(Material mat, Color start, Color end, float duration)
+    {
+        for (float t = 0f; t < duration; t += Time.deltaTime)
+        {
+            float normalizedTime = t / duration;
+//right here, you can now use normalizedTime as the third parameter in any Lerp from start to end
+            mat.color = Color.Lerp(start, end, normalizedTime);
+            yield return null;
+        }
+
+        mat.color = end; //without this, the value will end at something like 0.9992367
+    }
+
+    public void FadeToZero(List<Material> mats, int index)
+    {
+        StartCoroutine(DoAThingOverTime(mats[index], mats[index].color, new Color(mats[index].color.r,mats[index].color.g,mats[index].color.b,0), 1));
+
+        //set material color alpha to 0
+    }
+
+    public void FadeToOne(List<Material> mats, int index)
+    {
+        StartCoroutine(DoAThingOverTime(mats[index], mats[index].color, new Color(mats[index].color.r,mats[index].color.g,mats[index].color.b,1), 1));
+        //set material color alpha to 1
+    }
+    
 
     public void Next()
     {
@@ -25,6 +56,8 @@ public class ThingsToHide : MonoBehaviour
             Hide(cycleState);
         }
     }
+    
+    
 
     public void Prev()
     {
@@ -52,6 +85,7 @@ public class ThingsToHide : MonoBehaviour
                 part.SetActive(true);
             }
         }
+
         foreach (var part in texts)
         {
             if (part != texts[index])
@@ -63,6 +97,5 @@ public class ThingsToHide : MonoBehaviour
                 part.SetActive(true);
             }
         }
-        
     }
 }
